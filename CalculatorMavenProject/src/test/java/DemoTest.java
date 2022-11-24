b3c24a11e91d8a3fc3060b92785106506b2685ca
Jan Albert <jan.albert@pulselive.com>
2022-11-24 17:27:25 +0000

    init project

---
+++ CalculatorMavenProject/src/test/java/DemoTest.java
@@ -0,0 +1,16 @@
+import org.junit.jupiter.api.Test;
+
+/**
+ * Please add your description here.
+ *
+ * @author Pulse Innovations Ltd
+ */
+
+public class DemoTest
+{
+    @Test
+    void testDemo()
+    {
+        System.out.println( "Demo Test" );
+    }
+}

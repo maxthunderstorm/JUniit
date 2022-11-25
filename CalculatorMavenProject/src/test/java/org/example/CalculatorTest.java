07c45d9e62639acc7297d563d7fc19f47d01cdec
Jan Albert <jan.albert@pulselive.com>
2022-11-25 16:46:54 +0000

    use of nameconvention and @DisplayName

--- CalculatorMavenProject/src/test/java/org/example/CalculatorTest.java
+++ CalculatorMavenProject/src/test/java/org/example/CalculatorTest.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -9,16 +10,28 @@ import static org.junit.jupiter.api.Assertions.*;
  *
  * @author Pulse Innovations Ltd
  */
+
+@DisplayName( "Test math operations in Calculator class" )
 class CalculatorTest
 {
+    // test<System Under Test>_<Condition or State Change>_<Expected Result>
+    @DisplayName( "Test 4/2 = 2" )
     @Test
-    void integerDivsion()
+    void testIntegerDivision_WhenFourIsDividedByTwo_ShouldReturnTwo()
     {
         Calculator calculator = new Calculator();
         int result = calculator.integerDivision( 4, 2 );
         assertEquals( 2, result, "4/2 did not produce 2" );
     }
 
+    @DisplayName( "Division by zero" )
+    @Test
+    void testIntegerDivision_WhenDividendIsDividedByZero_ShouldThrowArithmeticException()
+    {
+        fail( "Not implemented yet" );
+    }
+
+    @DisplayName( "Test 33-1=32" )
     @Test
     void integerSubtraction()
     {

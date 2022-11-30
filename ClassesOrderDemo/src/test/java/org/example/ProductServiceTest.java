b962577b3f9f8f470665527d040a54007707a880
Jan Albert <jan.albert@pulselive.com>
2022-11-30 16:24:45 +0000

    Execution Order for Classes

--- ClassesOrderDemo/src/test/java/org/example/ProductServiceTest.java
+++ ClassesOrderDemo/src/test/java/org/example/ProductServiceTest.java
@@ -1,8 +1,10 @@
 package org.example;
 
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 
+@Order( 2 )
 public class ProductServiceTest
 {
     @BeforeAll
--- ClassesOrderDemo/src/test/java/org/example/UserServiceTest.java
+++ ClassesOrderDemo/src/test/java/org/example/UserServiceTest.java
@@ -1,8 +1,10 @@
 package org.example;
 
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 
+@Order( 1 )
 public class UserServiceTest
 {
     @BeforeAll

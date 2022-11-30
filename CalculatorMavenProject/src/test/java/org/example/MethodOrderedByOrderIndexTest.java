737bedb8e6265c13333927a105622e7da4e45095
Jan Albert <jan.albert@pulselive.com>
2022-11-30 16:42:31 +0000

    Test Instance Lifecycle: Test Instance per Class (shared attributes)

--- CalculatorMavenProject/src/test/java/org/example/MethodOrderedByOrderIndexTest.java
+++ CalculatorMavenProject/src/test/java/org/example/MethodOrderedByOrderIndexTest.java
@@ -1,18 +1,32 @@
 package org.example;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.TestMethodOrder;
 
+//@TestInstance( TestInstance.Lifecycle.PER_METHOD ) Default behaviour
+@TestInstance( TestInstance.Lifecycle.PER_CLASS )
 @TestMethodOrder( MethodOrderer.OrderAnnotation.class )
 public class MethodOrderedByOrderIndexTest
 {
+
+    private StringBuilder stringBuilder = new StringBuilder("");
+
+    @AfterEach
+    void afterEach()
+    {
+        System.out.println("The state of instance object is: " + stringBuilder);
+    }
+
     @Order( 1 )
     @Test
     void testD()
     {
         System.out.println( "Running Test D" );
+        stringBuilder.append( "1" );
     }
 
     @Order( 2 )
@@ -20,6 +34,7 @@ public class MethodOrderedByOrderIndexTest
     void testA()
     {
         System.out.println( "Running Test A" );
+        stringBuilder.append( "2" );
     }
 
     @Order( 3 )
@@ -27,6 +42,7 @@ public class MethodOrderedByOrderIndexTest
     void testC()
     {
         System.out.println( "Running Test C" );
+        stringBuilder.append( "3" );
     }
 
     @Order( 4 )
@@ -34,5 +50,6 @@ public class MethodOrderedByOrderIndexTest
     void testB()
     {
         System.out.println( "Running Test B" );
+        stringBuilder.append( "4" );
     }
 }

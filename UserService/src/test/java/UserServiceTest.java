6da94535391573262f009528dec3f39f9c76ef61
Jan Albert <jan.albert@pulselive.com>
2022-12-02 16:33:44 +0000

    UserService for TDD init

---
+++ UserService/src/test/java/UserServiceTest.java
@@ -0,0 +1,15 @@
+import org.junit.jupiter.api.Test;
+
+public class UserServiceTest
+{
+    @Test
+    void testCreateUser_whenUserDetailsProvided_returnUserObject()
+    {
+        //Given
+
+        //When
+
+        //Then
+    }
+
+}
